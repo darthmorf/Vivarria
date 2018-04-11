78d27bafd2143bcdfcd14250689b21dbb6b9c981
darthmorf <sampoirier@email.com>
2018-04-11 20:05:30 +0100

    Added tile asset variation

---
+++ Assets/Scripts/TileSprite.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSprite : MonoBehaviour {
+
+	public Sprite[] sprites;
+
+	void Start () {
+		int spriteIndex = Random.Range(0, sprites.Length - 1);
+		gameObject.GetComponent<SpriteRenderer>().sprite = sprites[spriteIndex];
+	}
+
+}
