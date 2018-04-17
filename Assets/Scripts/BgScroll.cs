1b2a9830ae84a21c6d6a2c4ae4004c1e6800e6c6
darthmorf <sampoirier@email.com>
2018-04-17 22:26:50 +0100

    Added BG parallax

---
+++ Assets/Scripts/BgScroll.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgScroll : MonoBehaviour {
+
+	public float speedX;
+	public float speedY;
+	Vector3 oldPos;
+	GameObject player;
+
+	private void Start()
+	{
+		player = GameObject.Find("Player");
+		oldPos = player.transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		float deltaX = player.transform.position.x - oldPos.x;
+		float deltaY = player.transform.position.y - oldPos.y;
+
+		Vector3 currentPos = transform.position;
+		currentPos.x = currentPos.x + deltaX * speedX;
+		currentPos.y = currentPos.y + deltaY * speedY;
+
+		transform.position = currentPos;
+		oldPos = player.transform.position;
+	}
+}
